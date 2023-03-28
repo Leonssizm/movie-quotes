6cb6857c0ae0595b6e3c894fa315c94d4280a93b
Leonssizm <levan.kereselidze@redberry.ge>
2023-03-28 13:54:32 +0400

    initial commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Movie Quotes</title>
+    </head>
+    <body class="bg-[radial-gradient(50%_50%_at_50%_50%,#4E4E4E_0%,#3D3B3B_99.99%,#3D3B3B_100%)]">
+        <h1 class="bg-gray-300">Hello World</h1>
+    </body>
+</html>
